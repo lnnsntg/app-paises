da71ca86bd889063535a30c0b4c81367053bd1ee
lenin <lnnsntg@gmail.com>
2021-05-14 17:53:58 +0200

    Fin video 101

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PaisModule,
+    SharedModule,
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
