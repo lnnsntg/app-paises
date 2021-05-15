5b11a81c7bb1d250b81a9a872d47dfcad2956ebd
lenin <lnnsntg@gmail.com>
2021-05-15 03:19:07 +0200

    Fin de video 106

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
-import { SharedModule } from './shared/shared.module';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     PaisModule,
     SharedModule,
-    AppRoutingModule
-
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[]= []
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+
+
+
+
+  constructor(private paisService: PaisService) {}
+
+  buscar() {
+    this.hayError = false;
+    console.log(this.termino);
+    this.paisService.buscarPais(this.termino).subscribe(
+      (paises) => {
+        console.log(paises);
+        this.paises = paises;
+      
+      },
+      (err) => {
+        this.hayError = true;
+        this.paises = [];
+      }
+    );
   }
-
 }
